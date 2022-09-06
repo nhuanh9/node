722e692628fea4039ec7152ec60b426cc5562b39
nhuanh9 <chopperlouis@gmail.com>
2022-09-06 10:19:14 +0700

    done b2

--- src/components/hooks/index.js
+++ src/components/hooks/index.js
@@ -1,26 +1,20 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
-const carList = ['Audi', 'MG', 'Honda', 'BMW'];
-const colorList = ['blue', 'red', 'green', 'black'];
 export default function HooksExercise() {
-    const [selectedCar, setSelectedCar] = useState({car: carList[0], color: colorList[0]});
+    const [timer, setTimer] = useState(3);
+    useEffect(() => {
+        let timeout = setTimeout(() => {
+            if (timer > 0) {
+                setTimer(timer - 1);
+            }
+        }, 1000)
+        if (timer == 0) {
+            clearTimeout(timeout)
+        }
+    }, [timer])
     return (
         <div>
-            <h1>{selectedCar.car} màu {selectedCar.color}</h1>
-            <select onChange={(e)=> {
-                setSelectedCar({...selectedCar, car: e.target.value})
-            }}>
-                {carList.map((car, index) => (
-                    <option key={index} value={car}>{car}</option>
-                ))}
-            </select>
-            <select  onChange={(e)=> {
-                setSelectedCar({...selectedCar, color: e.target.value})
-            }}>
-                {colorList.map(color => (
-                    <option value={color}>{color}</option>
-                ))}
-            </select>
+            {timer}
         </div>
     )
 }
