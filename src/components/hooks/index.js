1dedfdc396d992e744d1e9973cf0d670cced3fc0
nhuanh9 <chopperlouis@gmail.com>
2022-09-06 10:30:45 +0700

    doneb 3

--- src/components/hooks/index.js
+++ src/components/hooks/index.js
@@ -1,20 +1,40 @@
 import React, {useEffect, useState} from "react";
 
 export default function HooksExercise() {
-    const [timer, setTimer] = useState(3);
-    useEffect(() => {
-        let timeout = setTimeout(() => {
-            if (timer > 0) {
-                setTimer(timer - 1);
-            }
-        }, 1000)
-        if (timer == 0) {
-            clearTimeout(timeout)
-        }
-    }, [timer])
+
     return (
         <div>
-            {timer}
+            <Counter1></Counter1>
+            <Counter2></Counter2>
         </div>
     )
+}
+
+function Counter1() {
+    const [count, setCount] = useIncrement(1);
+    return (
+        <div>
+            <p>{count}</p>
+            <button onClick={setCount}>Add 1</button>
+        </div>
+    )
+}
+
+function Counter2() {
+    const [count, setCount] = useIncrement(2);
+    return (
+        <div>
+            <p>{count}</p>
+            <button onClick={setCount}>Add 2</button>
+        </div>
+    )
+}
+
+function useIncrement(props) {
+    const addAmount = props;
+    const [count, setCount] = useState(0);
+    const increase = () => {
+        setCount(count + addAmount);
+    }
+    return [count, increase]
 }
